9a6373376f58f0462e2f61a4f72a838a604e02f7
ParhomEsmaeili <parhom.esmaeili@gmail.com>
2025-11-19 13:10:40 +0000

    Updated configurations for the table generation bashscript

--- offline_result_summarisation/table_generation/table_generator_run.sh
+++ offline_result_summarisation/table_generation/table_generator_run.sh
@@ -2,14 +2,19 @@
 
 # Set your arguments here
 REFERENCE_METRICS_ROOT="/home/parhomesmaeili/IS-Validation-Framework/MIDL_DGX_Results_Summary"
-AXIS_OF_COMPLEXITY_NAME="Algorithm Complexity 1"
+AXIS_OF_COMPLEXITY_NAME="All Tasks Visualised"
 OUTPUT_ROOT="/home/parhomesmaeili/IS-Validation-Framework/MIDL_DGX_PrintedResults/${AXIS_OF_COMPLEXITY_NAME}"
 CAPTION="${AXIS_OF_COMPLEXITY_NAME} Comparison Table"
 LABEL="tab:${AXIS_OF_COMPLEXITY_NAME}_comparison"
 
 APPS=("sammed2dv1" "sam2v1" "sammed3dv1" "segvolv1"  "nnintv1")
 
-DATASET_NAMES=("Dataset004_Hippocampus" "Dataset001_BrainTumour" "Dataset007_Pancreas"  "Dataset003_Liver" )
+# DATASET_NAMES=("Dataset004_Hippocampus" "Dataset001_BrainTumour" "Dataset007_Pancreas"  "Dataset003_Liver" )
+# DATASET_NAMES=("Dataset005_Prostate" "Dataset001_BrainTumour") 
+# DATASET_NAMES=("Dataset005_Prostate" "Dataset001_BrainTumour" "Dataset008_HepaticVessel")
+# DATASET_NAMES=("Dataset006_Lung" "Dataset007_Pancreas" "Dataset003_Liver")
+# DATASET_NAMES=("Dataset003_Liver" "Dataset010_Colon")
+DATASET_NAMES=("Dataset001_BrainTumour" "Dataset003_Liver" "Dataset004_Hippocampus" "Dataset005_Prostate" "Dataset006_Lung" "Dataset007_Pancreas" "Dataset008_HepaticVessel" "Dataset010_Colon")
 PROMPTER="pointsonly"
 # RUN_NUMS=("1" "2" "3")
 RUN_NAME="-aggregated"
